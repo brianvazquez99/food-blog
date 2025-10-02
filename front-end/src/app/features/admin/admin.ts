1f498703ad2f61c7712fd79837d1c444f2a00616
Brian <brianvazquez@live.com>
2025-10-02 19:36:30 -0400

    adding buttons to admin screen

--- front-end/src/app/features/admin/admin.ts
+++ front-end/src/app/features/admin/admin.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import {QuillModule} from 'ngx-quill'
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
 
 @Component({
   selector: 'app-admin',
-  imports: [QuillModule],
+  imports: [QuillModule, FormsModule],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
   standalone: true,
 })
 export class Admin {
+
+  title = signal<string | undefined>(undefined)
+  body = signal<string | undefined>(undefined)
+
+  onSave() {
+    console.log("title", this.title())
+    console.log("body", this.body())
+  }
 
 }
