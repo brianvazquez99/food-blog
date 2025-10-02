f4f5c8024f468f3edaa3ac65d8fcbe1604e3afd6
Brian <brianvazquez@live.com>
2025-10-02 19:36:59 -0400

    blog post route WIP

--- main.go
+++ main.go
@@ -50,3 +50,41 @@ func main() {
 
 	 r.Run(":8080")
 }
+
+
+func uploadBlog(g *gin.Context, db *sql.DB) {
+	type BLOG_POST struct {
+		TITLE string
+		BODY string
+	}
+
+	var post BLOG_POST
+
+	err := g.ShouldBindJSON(&post)
+
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"message": "Incorrect format to post blog!"})
+	}
+
+	 query := `INSERT INTO BLOG-POSTS (TITLE, BODY)
+	 			VALUES (?,?)`
+
+
+	result, err := db.Exec(query, post.TITLE, post.BODY)
+
+	if err != nil {
+		g.JSON(http.StatusInternalServerError, gin.H{"message": "an error occured inserting the blog"})
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		g.JSON(http.StatusInternalServerError, gin.H{"message": "an error occured trying to get the id"})
+
+	}
+
+			g.JSON(http.StatusOK, gin.H{"ID": id})
+
+
+
+}
