42965c479534e4a45aef57b0e15521244f593526
Brian <brianvazquez@live.com>
2026-01-04 11:14:16 -0500

    adding functionality to tag recipe

--- front-end/src/app/features/admin/admin.ts
+++ front-end/src/app/features/admin/admin.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { QuillModule } from 'ngx-quill';
+import { QuillEditorComponent, QuillModule } from 'ngx-quill';
 import { BlogService } from '../../blog-service';
 import { RECIPE } from '../../types';
 import { RecipeDetails } from '../recipes/recipe-details/recipe-details';
+
+
+import './quill-recipe-blot'
 
 @Component({
   selector: 'app-admin',
@@ -16,7 +19,7 @@ import { RecipeDetails } from '../recipes/recipe-details/recipe-details';
     changeDetection:ChangeDetectionStrategy.OnPush
 
 })
-export class Admin  {
+export class Admin   {
 
   post = signal<RECIPE>({
     ID: null,
@@ -28,14 +31,13 @@ export class Admin  {
   })
 
 
+
   showToast = signal<boolean>(false)
   thumbnail!: File;
 
   imgSrc = signal<any>(null)
 
-    test = effect(() => {
-    console.log(this.post().BODY)
-  })
+
 
   previewOn = signal<boolean>(false)
 
@@ -48,6 +50,99 @@ export class Admin  {
   selectedCategories = signal<string[] >([])
 
   showCategories = signal<boolean>(false)
+
+
+  quillConfig = {
+    //toolbar: '.toolbar',
+    toolbar: {
+      container: [
+        ['italic', 'underline', 'strike'], // toggled buttons
+        ['code-block'],
+        [{ header: 1 }, { header: 2 }], // custom button values
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean'], // remove formatting button
+
+        ['link'],
+        ['BTN'],
+        [ 'image', 'video'],
+      ],
+      handlers: {
+        BTN:  () => {
+          const range = this.quill.getSelection()
+
+          if (!range) return;
+
+
+
+          const lines = this.quill.getLines(range.index, range.length);
+
+          console.log(lines)
+
+// Extract text or HTML per line
+const htmlLines = lines.map((line:any) => {
+  const text = line.domNode.innerHTML; // preserves <br> and inline formatting
+  return `<p>${text}</p>`;
+}).join('');
+
+console.log(htmlLines)
+
+// Delete the selected range
+this.quill.deleteText(range.index -3, range.length+3);
+
+
+this.quill.insertEmbed(range.index, 'recipe', htmlLines, 'user');
+
+        },
+      },
+    },
+  }
+
+  editor = viewChild<ElementRef<QuillEditorComponent>>("editor")
+
+
+  quill:any
+
+  // use this to change button title
+  populateBtn(quill:any) {
+    // Update your dropdown with labels
+    // let placeholderPickerItems =
+    //   // this.editor()!.nativeElement.querySelectorAll('.ql-csbtn ');
+    //   console.log(this.editor()?.nativeElement.)
+
+    // this.editor()!.nativeElement.querySelector('.ql-csbtn').innerHTML = 'BTN';
+
+    // console.log(this.editor()!.nativeElement.querySelectorAll('.ql-csbtn '))
+
+    this.quill = quill
+
+    console.log(this.quill)
+    const recipeBtn = document.querySelector('.ql-BTN') as HTMLElement;
+    console.log(recipeBtn)
+    if (recipeBtn) {
+      recipeBtn.innerHTML = 'Recipe';
+      recipeBtn.style.width = 'auto'; // Prevent it from being a tiny square
+    recipeBtn.style.padding = '0 8px';
+      // Or use an icon: recipeBtn.innerHTML = '<i class="fa fa-utensils"></i>';
+    }
+  }
+
+  onEditorCreated(quill:any) {
+    this.quill = quill
+  }
+
+  markRecipe() {
+
+  }
 
 
 
@@ -105,18 +200,18 @@ export class Admin  {
     formData.append('THUMBNAIL', this.thumbnail)
     formData.append('CATEGORY', this.selectedCategories().join())
 
-    this.http.post("/api/postBlog", formData).subscribe( {
-      next: value => {
-        this.showToast.set(true)
-        setTimeout(() => {
+    // this.http.post("/api/postBlog", formData).subscribe( {
+    //   next: value => {
+    //     this.showToast.set(true)
+    //     setTimeout(() => {
 
-          this.router.navigate(['recipe', this.blogService.getSlug(this.post().TITLE!)])
-        }, 3000);
-      },
-      error: err => {
-        console.log(err)
-      }
-    })
+    //       this.router.navigate(['recipe', this.blogService.getSlug(this.post().TITLE!)])
+    //     }, 3000);
+    //   },
+    //   error: err => {
+    //     console.log(err)
+    //   }
+    // })
   }
 
 }
