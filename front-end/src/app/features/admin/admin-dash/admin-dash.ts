cc4233949d1f9cf5744dcb2851a5fe178da961f9
Brian <brianvazquez@live.com>
2026-02-23 19:26:38 -0500

    adding verifytoken endpoint integration and hooking up get blogs endpoint to admin dash

--- front-end/src/app/features/admin/admin-dash/admin-dash.ts
+++ front-end/src/app/features/admin/admin-dash/admin-dash.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { BlogService } from '../../../blog-service';
 import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { tap } from 'rxjs';
+import { RECIPE } from '../../../types';
 
 @Component({
   selector: 'app-admin-dash',
@@ -15,26 +17,15 @@ export class AdminDash {
 
   // blogPosts = toSignal(this.blogService.getBlogList(), {initialValue:[]})
 
-  blogPosts = Array(5).fill(
-    {
-    ID: 20,
-    TITLE: 'test',
-    THUMBNAIL: new Blob(),
-    BODY: "ldfskkads;lfasdkfjasdfads",
-    DATE_ADDED: '1/26/2025',
-    SLUG: 'test',
-    CATEGORY: ['bread', 'holidfays']
-    }
-  )
-
-  copyPosts = [...this.blogPosts]
+  blogPosts = toSignal(this.blogService.getBlogList().pipe(tap(val => this.copyPosts.set(val))), {initialValue:[]})
+  copyPosts = signal<RECIPE[]>([])
 
   onSearch(search:Event) {
 
     const input = search.target as HTMLInputElement
     const searchString = input.value
     console.log(searchString)
-    const filtered = this.blogPosts.filter(el => {
+    const filtered = this.blogPosts().filter(el => {
       const title = el.TITLE as string
       const index = title.indexOf(searchString)
       if (index != -1) {
@@ -43,7 +34,7 @@ export class AdminDash {
 
     })
 
-    this.copyPosts = filtered
+    this.copyPosts.set([...filtered])
   }
 
 
--- front-end/src/app/features/admin/admin.ts
+++ front-end/src/app/features/admin/admin.ts
@@ -8,43 +8,45 @@ import { sign } from 'crypto';
   selector: 'app-admin',
   imports: [RouterOutlet, RouterLink, FormsModule],
   templateUrl: './admin.html',
-  styleUrl: './admin.css'
+  styleUrl: './admin.css',
 })
 export class Admin implements OnInit {
-
-  isLoggedIn = signal<boolean>(false)
-  http = inject(HttpClient)
-  password = signal<string | undefined>(undefined)
-  errorMessage = signal<string | undefined>(undefined)
-  isLoading = signal<boolean>(false)
+  isLoggedIn = signal<boolean>(false);
+  http = inject(HttpClient);
+  password = signal<string | undefined>(undefined);
+  errorMessage = signal<string | undefined>(undefined);
+  isLoading = signal<boolean>(false);
 
   ngOnInit(): void {
-    const token = localStorage.getItem("jwt-key")
-    if (token == null) {
-      this.isLoggedIn.set(false)
-    }
-    else {
+    this.http.get('/api/verifyAdmin').subscribe({ next: (value) => {
+      console.log(value)
       this.isLoggedIn.set(true)
-    }
+    },
+  error:(err) => {
+    console.log(err)
+    this.isLoggedIn.set(false)
+  }
+
+});
+
   }
 
   onLogin() {
+    if (!this.password()) return;
 
-    if (!this.password()) return
+    this.isLoading.set(true);
 
-    this.isLoading.set(true)
-
-    this.http.post("/api/login", {PASSWORD:this.password()}, {withCredentials:true}).subscribe({
-      next:(value) => {
-       this.isLoading.set(false)
-       this.isLoggedIn.set(true)
-      },
-      error:(err) => {
-        this.errorMessage.set(err)
-        this.isLoading.set(false)
-      }
-    })
-
+    this.http
+      .post('/api/login', { PASSWORD: this.password() }, { withCredentials: true })
+      .subscribe({
+        next: (value) => {
+          this.isLoading.set(false);
+          this.isLoggedIn.set(true);
+        },
+        error: (err) => {
+          this.errorMessage.set(err);
+          this.isLoading.set(false);
+        },
+      });
   }
-
 }
